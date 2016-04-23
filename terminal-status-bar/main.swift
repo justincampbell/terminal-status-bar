1e94ad4565bcbba73c1f07f6b2c20793cb91de42
Justin Campbell <justin@justincampbell.me>
2016-04-23 14:47:31 -0400

    Create new Xcode CLI project

---
+++ terminal-status-bar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  terminal-status-bar
+//
+//  Created by Justin Campbell on 4/23/16.
+//  Copyright © 2016 Justin Campbell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
